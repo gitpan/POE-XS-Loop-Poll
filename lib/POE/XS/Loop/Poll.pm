b5847ebe93feed75fa18449f2df9a35cdd1bb58b
Tony Cook <tony@develop-help.com>
2008-07-03 01:15:10 +0000

    Import of TONYC/POE-XS-Loop-Poll-0.003 from CPAN.
    gitpan-cpan-distribution: POE-XS-Loop-Poll
    gitpan-cpan-version:      0.003
    gitpan-cpan-path:         TONYC/POE-XS-Loop-Poll-0.003.tar.gz
    gitpan-cpan-author:       TONYC
    gitpan-cpan-maturity:     released

--- lib/POE/XS/Loop/Poll.pm
+++ lib/POE/XS/Loop/Poll.pm
@@ -7,7 +7,7 @@ BEGIN {
     # noisy
     *POE::Kernel::TRACE_CALLS = sub () { 0 };
   }
-  $VERSION = '0.002';
+  $VERSION = '0.003';
   eval {
     # try XSLoader first, DynaLoader has annoying baggage
     require XSLoader;
